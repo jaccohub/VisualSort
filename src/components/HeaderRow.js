9eebb9d42e5a603d5d2d6be15ec8d6aac55941e0
Jacob Lodenius <jacoblodenius@gmail.com>
2020-11-03 22:20:42 +0100

    some header styling

--- src/components/HeaderRow.js
+++ src/components/HeaderRow.js
@@ -7,25 +7,28 @@ export default function HeaderRow({ start, reset, arraySize, setArraySize }) {
 
   return (
     <div className="header">
-      <select>
-        <option>Bubble sort</option>
-        <option disabled>Quick sort</option>
-      </select>
-      <button onClick={start}>
-        Start
-      </button>
-      <button onClick={reset}>
-        Reset
-      </button>
-      <input
-        onChange={onChange}
-        type="range"
-        min="5"
-        max="100"
-        defaultValue={arraySize}
-        className="slider"
-        id="slider"
-      />
+      <h1>VisualSort</h1>
+      <div className="center">
+        <select>
+          <option>Bubble sort</option>
+          <option disabled>Quick sort</option>
+        </select>
+        <button onClick={start}>
+          Start
+        </button>
+        <button onClick={reset}>
+          Reset
+        </button>
+        <input
+          onChange={onChange}
+          type="range"
+          min="5"
+          max="100"
+          defaultValue={arraySize}
+          className="slider"
+          id="slider"
+        />
+      </div>
     </div>
   )
 }
