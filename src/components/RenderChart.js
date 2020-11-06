ab917cfa7fe1269603fcccf3013bde520aa315ff
Jacob Lodenius <jacoblodenius@gmail.com>
2020-11-06 22:38:19 +0100

    refactor

--- src/components/RenderChart.js
+++ src/components/RenderChart.js
@@ -5,14 +5,12 @@ export default function RenderChart({ keyFrames, step }) {
   const { array, current, compare } = frame
 
   return (
-    <div>
-      <div className="container">
-        {array.map((height, index) => {
-          let className = 'bar'
-          if ([current, compare].includes(index)) className += ' arrow'
-          return <div key={index} className={className} style={{ height }} />
-        })}
-      </div>
+    <div className="chart">
+      {array.map((height, index) => {
+        let className = 'bar'
+        if ([current, compare].includes(index)) className += ' arrow'
+        return <div key={index} className={className} style={{ height }} />
+      })}
     </div>
   )
 }
